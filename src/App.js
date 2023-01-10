42889a0f574748aefd96f72e35a9ed86bbd3c0fc
irvanshandika <shandikamuhammadirvan@gmail>
2023-01-10 10:28:51 +0700

    Deleting Login

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import MainPage from "./pages/MainPage.js";
 import BlankPage from "./pages/BlankPage.js";
-import Login from "./pages/Login.js";
 import OlahragaPage from "./pages/OlahragaPage.js";
 import BisnisPage from "./pages/BisnisPage.js";
 import TeknologiPage from "./pages/TeknologiPage.js";
@@ -20,7 +19,6 @@ function App() {
         <Route path="/">
           <Route index element={<MainPage />} />
           <Route path="teampage" element={<TeamPage />} />
-          <Route path="login" element={<Login />} />
           <Route path="*" element={<BlankPage />} />
           <Route path="bisnis" element={<BisnisPage />} />
           <Route path="teknologi" element={<TeknologiPage />} />
